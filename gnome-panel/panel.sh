102052fe451132a5b2f0fc51b49b97ae1c7b836c
George Lebl <jirka@src.gnome.org>
1998-03-25 05:47:28 +0000

    small fix to the script -George

--- gnome-panel/panel.sh
+++ gnome-panel/panel.sh
@@ -2,7 +2,7 @@
 #this is a temporary script to launch the launcher and the panel
 
 #this will kill the launcher from before if it is still running
-kill `ps x|awk /[l]auncher_applet/ '{print $1}'`
+kill `ps x|awk '/[l]auncher_applet/ {print $1}'`
 
 launcher_applet &
 panel
