24bbc719e9549afd9120de23b09b767553405a11
Havoc Pennington <hp@pobox.com>
1998-05-09 02:01:49 +0000

    Added ability to name your GNOME Fish.
    Fri May  8 21:01:36 1998  Havoc Pennington  <hp@pobox.com>
    
    	* fish.c: Added ability to name your GNOME Fish.

--- applets/fish/fish.c
+++ applets/fish/fish.c
@@ -11,47 +11,160 @@
 #include "fish2.xpm"
 #include "fish3.xpm"
 
+typedef struct _fish_properties fish_properties;
+
+struct _fish_properties {
+  gchar * name;
+};
+
+static const fish_properties defaults = {
+  "Wanda"
+};
+
+static fish_properties properties = { NULL };
+
 static GtkWidget *applet;
 static GdkPixmap *pix[3];
 static GdkPixmap *mask[3];
 static gint curpix = 0;
+
+static GtkWidget * fortune_dialog = NULL;
+static GtkWidget * fortune_label;
+
+#define NAME_KEY "/fish_applet/general/name"
+
+static void
+load_properties()
+{
+  if (properties.name) g_free(properties.name);
+
+  properties.name = gnome_config_get_string(NAME_KEY);
+
+  if (properties.name == NULL) properties.name = g_strdup(defaults.name);
+}
+
+static gchar *
+splice_name(const gchar * format, const gchar * name)
+{
+  gchar * buf;
+  gint len;
+  len = strlen(name) + strlen(format);
+  buf = g_malloc(len+1);
+  g_snprintf(buf, len, format, name);
+  return buf;
+}
+
+static void
+apply_properties() 
+{
+  gchar * tmp;
+  const gchar * title_format = _("%s the Fish");
+  const gchar * label_format = _("%s the GNOME Fish Says:");
+
+  if ( fortune_dialog != NULL ) { 
+    tmp = splice_name(title_format, properties.name);
+    gtk_window_set_title(GTK_WINDOW(fortune_dialog), tmp);
+    g_free(tmp);
+    
+    tmp = splice_name(label_format, properties.name);
+    gtk_label_set(GTK_LABEL(fortune_label), tmp);
+    g_free(tmp);
+  }
+
+  gnome_config_set_string(NAME_KEY, properties.name);
+  gnome_config_sync();
+}
+
+static void
+apply_cb(GnomePropertyBox * pb, gint page, gpointer data)
+{
+  gchar * new_name;
+
+  if (page != -1) return; /* Only honor global apply */
+
+  new_name = gtk_entry_get_text(GTK_ENTRY(data));
+
+  if (new_name) {
+    g_free(properties.name);
+    properties.name = g_strdup(new_name);
+  }
+
+  apply_properties();
+}
+
+static void 
+properties_dialog(AppletWidget * w, gpointer data)
+{
+  GtkWidget * pb;
+  GtkWidget * vbox;
+  GtkWidget * entry;
+  GtkWidget * label;
+
+  pb = gnome_property_box_new();
+
+  gtk_window_set_title(GTK_WINDOW(pb), _("GNOME Fish Properties"));
+
+  vbox = gtk_vbox_new(GNOME_PAD, FALSE);
+  gtk_container_border_width(GTK_CONTAINER(vbox), GNOME_PAD);
+
+  label = gtk_label_new(_("Your GNOME Fish's Name:"));
+  entry = gtk_entry_new();
+
+  gtk_entry_set_text(GTK_ENTRY(entry), properties.name);
+
+  gtk_signal_connect_object(GTK_OBJECT(entry), "changed",
+			    GTK_SIGNAL_FUNC(gnome_property_box_changed),
+			    GTK_OBJECT(pb));
+
+  gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, GNOME_PAD);
+  gtk_box_pack_start(GTK_BOX(vbox), entry, TRUE, TRUE, GNOME_PAD);
+
+  gnome_property_box_append_page(GNOME_PROPERTY_BOX(pb), vbox,
+				 gtk_label_new(_("Name")));
+
+  gtk_signal_connect(GTK_OBJECT(pb), "apply", GTK_SIGNAL_FUNC(apply_cb),
+		     entry);
+
+  gtk_widget_show_all(pb);
+}
+
+static void 
+update_fortune_dialog()
+{
+  static GtkWidget * fortune_less = NULL; 
+
+  if ( fortune_dialog == NULL ) {
+    fortune_dialog = 
+      gnome_dialog_new("", GNOME_STOCK_BUTTON_CLOSE, NULL);
+    gnome_dialog_set_close(GNOME_DIALOG(fortune_dialog), TRUE);
+    gnome_dialog_close_hides(GNOME_DIALOG(fortune_dialog), TRUE);
+
+    fortune_less = gnome_less_new();
+    fortune_label = gtk_label_new("");
+    
+    gtk_box_pack_start(GTK_BOX(GNOME_DIALOG(fortune_dialog)->vbox), 
+		       fortune_label,
+		       FALSE, FALSE, GNOME_PAD);
+
+    gtk_box_pack_start(GTK_BOX(GNOME_DIALOG(fortune_dialog)->vbox), 
+		       fortune_less,
+		       TRUE, TRUE, GNOME_PAD);
+
+    gnome_less_fixed_font(GNOME_LESS(fortune_less));
+
+    gtk_widget_show(fortune_less);
+    gtk_widget_show(fortune_label);
+    apply_properties();
+  }
+  if (!GTK_WIDGET_VISIBLE(fortune_dialog)) gtk_widget_show(fortune_dialog);  
+  gnome_less_show_command(GNOME_LESS(fortune_less), "fortune");
+}
 
 static gint
 destroy_applet(GtkWidget *widget, gpointer data)
 {
 	gtk_exit(0);
 	return FALSE;
-}
-
-static void 
-update_fortune_dialog()
-{
-  static GtkWidget * dialog = NULL;
-  static GtkWidget * less = NULL; 
-  GtkWidget * label;
-
-  if ( dialog == NULL ) {
-    dialog = 
-      gnome_dialog_new(_("Wanda the Fish"), GNOME_STOCK_BUTTON_CLOSE, NULL);
-    gnome_dialog_set_close(GNOME_DIALOG(dialog), TRUE);
-    gnome_dialog_close_hides(GNOME_DIALOG(dialog), TRUE);
-
-    less = gnome_less_new();
-    label = gtk_label_new(_("Wanda the GNOME Fish Says:"));
-    
-    gtk_box_pack_start(GTK_BOX(GNOME_DIALOG(dialog)->vbox), label,
-		       FALSE, FALSE, GNOME_PAD);
-
-    gtk_box_pack_start(GTK_BOX(GNOME_DIALOG(dialog)->vbox), less,
-		       TRUE, TRUE, GNOME_PAD);
-
-    gnome_less_fixed_font(GNOME_LESS(less));
-
-    gtk_widget_show(less);
-    gtk_widget_show(label);
-  }
-  if (!GTK_WIDGET_VISIBLE(dialog)) gtk_widget_show(dialog);  
-  gnome_less_show_command(GNOME_LESS(less), "fortune");
 }
 
 static gint 
@@ -119,6 +232,8 @@ create_fish_widget(GtkWidget *window)
         gtk_container_add(GTK_CONTAINER(event_box),pixmap);
         gtk_container_add(GTK_CONTAINER(frame),event_box);
 
+	load_properties();
+
         return frame;
 }
 
@@ -127,11 +242,12 @@ void
 about_cb (AppletWidget *widget, gpointer data)
 {
 	GtkWidget *about;
-	gchar *authors[] = {
-	  "Wanda, the fish",
-	  "with minor help from George",
-	  NULL
-	  };
+	gchar *authors[3];
+	const gchar * author_format = _("%s the Fish");
+
+	authors[0] = splice_name(author_format, properties.name);
+	authors[1] = "(with minor help from George)";
+	authors[2] = NULL;
 
 	about = gnome_about_new ( "The GNOME Fish Applet", "3.4.7.4",
 			"(C) 1998 the Free Software Foundation",
@@ -145,6 +261,8 @@ about_cb (AppletWidget *widget, gpointer data)
 			"evaluation.",
 			NULL);
 	gtk_widget_show (about);
+
+	g_free(authors[0]);
 
 	return;
 }
@@ -174,9 +292,17 @@ main(int argc, char *argv[])
 
 	applet_widget_register_callback(APPLET_WIDGET(applet),
 					"about",
-					_("About"),
+					_("About..."),
 					about_cb,
 					NULL);
+
+
+	applet_widget_register_callback(APPLET_WIDGET(applet),
+					"properties",
+					_("Properties..."),
+					properties_dialog,
+					NULL);
+
 	
 	applet_widget_gtk_main();
 
