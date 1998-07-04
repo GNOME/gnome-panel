3e128dbad666c3b3db9ea54e8d632aeb938d0994
Carsten Schaar <carsten@src.gnome.org>
1998-07-04 15:29:01 +0000

    Now uses stock menus icons.
    * fish.c (main): Now uses stock menus icons.

--- applets/fish/fish.c
+++ applets/fish/fish.c
@@ -304,18 +304,20 @@ main(int argc, char *argv[])
 			   GTK_SIGNAL_FUNC(applet_save_session),
 			   NULL);
 
-	applet_widget_register_callback(APPLET_WIDGET(applet),
-					"about",
-					_("About..."),
-					about_cb,
-					NULL);
+	applet_widget_register_stock_callback(APPLET_WIDGET(applet),
+					      "about",
+					      GNOME_STOCK_MENU_ABOUT,
+					      _("About..."),
+					      about_cb,
+					      NULL);
 
 
-	applet_widget_register_callback(APPLET_WIDGET(applet),
-					"properties",
-					_("Properties..."),
-					properties_dialog,
-					NULL);
+	applet_widget_register_stock_callback(APPLET_WIDGET(applet),
+					      "properties",
+					      GNOME_STOCK_MENU_PROP,
+					      _("Properties..."),
+					      properties_dialog,
+					      NULL);
 
 	
 	applet_widget_gtk_main();
