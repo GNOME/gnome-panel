a9386c580ed23fd9d55a6dadf89193d896473791
Jacob Berkman <jberkman@andrew.cmu.edu>
1999-09-30 23:53:42 +0000

    don't allow back_pixmap to be NULL
    1999-09-30  Jacob Berkman  <jberkman@andrew.cmu.edu>
    
    * panel-widget.c (panel_widget_new): don't allow back_pixmap
    to be NULL

--- gnome-panel/panel-widget.c
+++ gnome-panel/panel-widget.c
@@ -1437,10 +1437,7 @@ panel_widget_new (int packed,
 	panel->back_type = back_type;
 
 	panel->fit_pixmap_bg = fit_pixmap_bg;
-	if(back_pixmap)
-		panel->back_pixmap = g_strdup(back_pixmap);
-	else
-		panel->back_pixmap = NULL;
+	panel->back_pixmap = g_strdup (back_pixmap ? back_pixmap : "");
 	
 	if(back_color)
 		panel->back_color = *back_color;
