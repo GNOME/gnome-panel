049a5a25efd630100b5ec1f805815e20bfe7ed0a
Jesus Bravo Alvarez <jesusb@src.gnome.org>
2000-01-18 23:08:08 +0000

    Fixed typo (missing ending parentheses) in menu.h

--- gnome-panel/menu.h
+++ gnome-panel/menu.h
@@ -104,7 +104,7 @@ enum {
 #define MENU_SIZE_SMALL "Small (36 pixels)"
 #define MENU_SIZE_STANDARD "Standard (48 pixels)"
 #define MENU_SIZE_LARGE "Large (64 pixels)"
-#define MENU_SIZE_HUGE "Huge (80 pixels"
+#define MENU_SIZE_HUGE "Huge (80 pixels)"
 
 #define MENU_BACKS "background_menu"
 #define MENU_BACK_NONE "Standard"
