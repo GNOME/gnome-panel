e63f97ea676b7eb7b4adfd80a421f63baf649e8e
Alexander Larsson <alla@lysator.liu.se>
2002-01-13 06:54:34 +0000

    Hook up the workspaces part of the properties dialog. This needs some
    2002-01-13  Alexander Larsson  <alla@lysator.liu.se>
    	* pager.c:
    	* pager.glade:
    	Hook up the workspaces part of the properties dialog.
    	This needs some loving in gnome-settings-daemon to work.

--- applets/gen_util/pager.c
+++ applets/gen_util/pager.c
@@ -12,6 +12,8 @@
 #include <panel-applet.h>
 #include <panel-applet-gconf.h>
 
+#include <stdlib.h>
+
 #include <gtk/gtk.h>
 #include <libbonobo.h>
 #include <libgnome/libgnome.h>
@@ -36,6 +38,10 @@ typedef struct {
 	GtkWidget *all_workspaces_radio;
 	GtkWidget *current_only_radio;
 	GtkWidget *num_rows_spin;
+	GtkWidget *num_workspaces_spin;
+	GtkWidget *workspaces_tree;
+
+	GtkListStore *workspaces_store;
 	
 	GtkOrientation orientation;
 	int n_rows;
@@ -177,8 +183,6 @@ display_workspace_names_changed (GConfClient *client,
 		value = gconf_value_get_bool (entry->value);
 	}
 
-	g_print ("display_workspace_names_changed, value=%d\n", value);
-
 	if (value) {
 		pager->display_mode = WNCK_PAGER_DISPLAY_NAME;
 	} else {
@@ -300,11 +304,10 @@ fill_pager_applet(PanelApplet *applet)
 		pager->display_all = TRUE; /* Default value */
 	}
 	
-	/* FIXME: We need to get the real initial panel data here */
 	pager->size = 48;
 	pager->orientation = GTK_ORIENTATION_HORIZONTAL;
 
-	/* FIXME: Needs to get the screen number from DISPLAY or the panel. */
+	/* TODO: Needs to get the screen number from DISPLAY or the panel. */
 	pager->screen = wnck_screen_get (0);
 
 	/* because the pager doesn't respond to signals at the moment */
@@ -352,7 +355,7 @@ display_help_dialog (BonoboUIComponent *uic,
 		     PagerData         *pager,
 		     const gchar       *verbname)
 {
-	/* FIXME: Implement this */
+	/* TODO: Implement this */
 }
 
 static void
@@ -378,7 +381,7 @@ display_about_dialog (BonoboUIComponent *uic,
 
 	pixbuf = NULL;
 
-	/* FIXME: This should not use gnome-clock! */
+	/* TODO: This should not use gnome-clock! */
 	file = gnome_program_locate_file (NULL, GNOME_FILE_DOMAIN_PIXMAP, "gnome-clock.png", TRUE, NULL);
 	if (!file) {
 		g_warning (G_STRLOC ": gnome-clock.png cannot be found");
@@ -413,7 +416,6 @@ static void
 display_workspace_names_toggled (GtkToggleButton *button,
 				 PagerData       *pager)
 {
-	g_print ("display_workspace_names_toggled, widget state:%d\n", gtk_toggle_button_get_active (button));
 	panel_applet_gconf_set_bool (PANEL_APPLET (pager->applet),
 				     "display_workspace_names",
 				     gtk_toggle_button_get_active (button),
@@ -440,7 +442,89 @@ num_rows_value_changed (GtkSpinButton *button,
 				    NULL);
 }
 
+static void
+update_workspaces_model (PagerData *pager)
+{
+	int nr_ws, i;
+	WnckWorkspace *workspace;
+	GtkTreeIter iter;
+	
+	nr_ws = wnck_screen_get_workspace_count (pager->screen);
+	
+	gtk_spin_button_set_value (GTK_SPIN_BUTTON (pager->num_workspaces_spin), nr_ws);
 
+	gtk_list_store_clear (pager->workspaces_store);
+	for (i = 0; i < nr_ws; i++) {
+		workspace = wnck_workspace_get (i);
+		gtk_list_store_append (pager->workspaces_store, &iter);
+		gtk_list_store_set (pager->workspaces_store,
+				    &iter,
+				    0, wnck_workspace_get_name (workspace),
+				    -1);
+	}
+}
+
+static void
+workspace_created_or_destroyed (WnckScreen *screen,
+				WnckWorkspace *space,
+				PagerData *pager)
+{
+	update_workspaces_model (pager);
+}
+
+static void
+num_workspaces_value_changed (GtkSpinButton *button,
+			      PagerData       *pager)
+{
+	GConfClient* client;
+	
+	client = gconf_client_get_default ();
+
+	gconf_client_set_int (client, "/desktop/gnome/applications/window_manager/nr_of_workspaces",
+			      gtk_spin_button_get_value_as_int (GTK_SPIN_BUTTON (pager->num_workspaces_spin)), NULL);
+}
+
+static void 
+workspace_name_edited (GtkCellRendererText *cell_renderer_text,
+		       const gchar         *path,
+		       const gchar         *new_text,
+		       PagerData           *pager)
+{
+	GConfClient* client;
+	GtkTreeIter iter;
+	char *str;
+	GSList *list;
+
+	if (gtk_tree_model_get_iter_from_string (GTK_TREE_MODEL (pager->workspaces_store),
+						 &iter, path)) {
+		gtk_list_store_set (pager->workspaces_store,
+				    &iter,
+				    0, new_text,
+				    -1);
+	}
+
+	
+	list = NULL;
+	gtk_tree_model_get_iter_root (GTK_TREE_MODEL (pager->workspaces_store), &iter);
+	do {
+		gtk_tree_model_get (GTK_TREE_MODEL (pager->workspaces_store),
+				    &iter,
+				    0, &str,
+				    -1);
+		list = g_slist_prepend (list, str);
+	} while (gtk_tree_model_iter_next (GTK_TREE_MODEL (pager->workspaces_store), &iter));
+	
+	list = g_slist_reverse (list);
+
+	client = gconf_client_get_default ();
+	gconf_client_set_list (client,  "/desktop/gnome/applications/window_manager/workspace_names",
+			       GCONF_VALUE_STRING,
+			       list,
+			       NULL);
+
+	g_slist_foreach (list, (GFunc)g_free, NULL);
+	g_slist_free (list);
+}
 
 static void
 setup_dialog (GladeXML  *xml,
@@ -448,13 +532,17 @@ setup_dialog (GladeXML  *xml,
 {
 	gboolean value;
 	GConfClient *client;
-
+	GtkTreeViewColumn *column;
+	GtkCellRenderer *cell;
+	
 	client = gconf_client_get_default ();
 
 	pager->display_workspaces_toggle = WID ("workspace_name_toggle");
 	pager->all_workspaces_radio = WID ("all_workspaces_radio");
 	pager->current_only_radio = WID ("current_only_radio");
 	pager->num_rows_spin = WID ("num_rows_spin");
+	pager->num_workspaces_spin = WID ("num_workspaces_spin");
+	pager->workspaces_tree = WID ("workspaces_tree_view");
 
 	/* Display workspace names: */
 	
@@ -490,6 +578,28 @@ setup_dialog (GladeXML  *xml,
 	
 	g_signal_connect_swapped (WID ("done_button"), "pressed",
 				  (GCallback) gtk_widget_hide, pager->properties_dialog);
+
+	gtk_spin_button_set_value (GTK_SPIN_BUTTON (pager->num_workspaces_spin),
+				   wnck_screen_get_workspace_count (pager->screen));
+	g_signal_connect (G_OBJECT (pager->num_workspaces_spin), "value_changed",
+			  (GCallback) num_workspaces_value_changed, pager);
+	
+	g_signal_connect_swapped (pager->screen, "workspace_created",
+				  (GCallback) workspace_created_or_destroyed, pager);
+	g_signal_connect_swapped (pager->screen, "workspace_destroyed",
+				  (GCallback) workspace_created_or_destroyed, pager);
+
+	pager->workspaces_store = gtk_list_store_new (1, G_TYPE_STRING, NULL);
+	update_workspaces_model (pager);
+	gtk_tree_view_set_model (GTK_TREE_VIEW (pager->workspaces_tree), GTK_TREE_MODEL (pager->workspaces_store));
+	cell = g_object_new (GTK_TYPE_CELL_RENDERER_TEXT, "editable", TRUE, NULL);
+	column = gtk_tree_view_column_new_with_attributes ("workspace",
+							   cell,
+							   "text", 0,
+							   NULL);
+	gtk_tree_view_append_column (GTK_TREE_VIEW (pager->workspaces_tree), column);
+	g_signal_connect (cell, "edited",
+			  (GCallback) workspace_name_edited, pager);
 }
 
 static void 
@@ -509,6 +619,4 @@ display_properties_dialog (BonoboUIComponent *uic,
 	}
 
 	gtk_window_present (GTK_WINDOW (pager->properties_dialog));
-	
-	/* FIXME: Implement this. Should only need n_rows */
 }
