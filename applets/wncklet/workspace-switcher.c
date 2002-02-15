efce02a20f8f00d99cf597faa9d5e3e11ff61733
Seth Nickell <snickell@stanford.edu>
2002-02-15 03:54:52 +0000

    Add better descriptions.
    2002-02-14  Seth Nickell  <snickell@stanford.edu>
    
    	* GNOME_GenUtilApplet_Factory.server.in.in:
    
    	Add better descriptions.
    
    	* pager.c: (display_about_dialog):
    	* pager.glade:
    
    	Use "Workspace Switcher", not "Workspace List",
    	and remove unnecessary verbosity.CVS: ----------------------------------------------------------------------

--- applets/wncklet/workspace-switcher.c
+++ applets/wncklet/workspace-switcher.c
@@ -408,9 +408,9 @@ display_about_dialog (BonoboUIComponent *uic,
 		pixbuf = gdk_pixbuf_new_from_file (file, NULL);
 	}
 
-	about = gnome_about_new (_("Workspace List"), "1.0",
+	about = gnome_about_new (_("Workspace Switcher"), "1.0",
 				 _("(c) 2001 Red Hat, Inc."),
-				 _("The Workspace List shows you a small version of your workspaces that lets you manage the windows."),
+				 _("The Workspace Switcher shows you a small version of your workspaces that lets you manage the windows."),
 				 authors,
 				 NULL, /* documenters */
 				 NULL, /* translator_credits */
