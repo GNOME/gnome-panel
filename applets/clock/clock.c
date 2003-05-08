25b342e04f08eb11bda43a800d9147b2bd67e313
Mark McLoughlin <mark@skynet.ie>
2003-05-08 15:37:54 +0000

    re-instate some authors who were mysteriously deleted.
    2003-05-08  Mark McLoughlin  <mark@skynet.ie>
    
            * clock.c: re-instate some authors who were mysteriously
            deleted.

--- applets/clock/clock.c
+++ applets/clock/clock.c
@@ -1,10 +1,32 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
 /*
- * GNOME time/date display module.
- * (C) 1997 The Free Software Foundation
+ * clock.c: the GNOME clock applet
  *
- * Authors: George Lebl
- * 	    Gediminas Paulauskas
+ * Copyright (C) 1997-2003 Free Software Foundation, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Authors:
+ *      Miguel de Icaza
+ *      Frederico Mena
+ *      Stuart Parmenter
+ *      Alexander Larsson
+ *      George Lebl
+ *      Gediminas Paulauskas
+ *      Mark McLoughlin
  */
 
 #include "config.h"
