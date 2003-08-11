86a85bb42fb4c28e916cfd388e115a8d7b358e06
George Lebl <jirka@5z.com>
2003-08-11 18:46:18 +0000

    when queing unhide, first check if the panel contains the pointer before
    Mon Aug 11 11:40:00 2003  George Lebl <jirka@5z.com>
    
    	* panel-toplevel.c: when queing unhide, first check if the panel
    	  contains the pointer before whacking the unhide delay, this fixes
    	  bugs where panel sometimes would never pop up.

--- gnome-panel/panel-toplevel.c
+++ gnome-panel/panel-toplevel.c
@@ -3117,21 +3117,15 @@ panel_toplevel_queue_auto_hide (PanelToplevel *toplevel)
 {
 	g_return_if_fail (PANEL_IS_TOPLEVEL (toplevel));
 
-	if (!toplevel->priv->auto_hide)
-		return;
-
-	if (toplevel->priv->hide_timeout)
+	if ( ! toplevel->priv->auto_hide ||
+	    toplevel->priv->hide_timeout ||
+	    toplevel->priv->state != PANEL_STATE_NORMAL ||
+	    panel_toplevel_contains_pointer (toplevel))
 		return;
 
 	if (toplevel->priv->unhide_timeout)
 		g_source_remove (toplevel->priv->unhide_timeout);
 	toplevel->priv->unhide_timeout = 0;
-
-	if (toplevel->priv->state != PANEL_STATE_NORMAL)
-		return;
-
-	if (panel_toplevel_contains_pointer (toplevel))
-		return;
 
 	if (toplevel->priv->block_auto_hide) {
 		/* Since this will continue to be called until
