a3076ee1037cecd60080eab4adc37db88c776768
Vincent Untz <vincent@vuntz.net>
2003-11-20 09:14:39 +0000

    do not stop unhiding the panel if the cursor is not in the panel but the
    2003-11-20  Vincent Untz  <vincent@vuntz.net>
    
    	* panel-toplevel.c: (panel_toplevel_auto_unhide_timeout_handler): do
    	not stop unhiding the panel if the cursor is not in the panel but the
    	auto_hide property is FALSE. Fix behaviour when you changed the
    	auto_hide key in GConf and the panel did not unhide.
    	(panel_toplevel_hide), (panel_toplevel_unhide): if the animation is
    	disabled, do gtk_widget_hide/show only if the toplevel is a drawer.
    	This fix a bug where a panel was totally hidden and the user could not
    	bring it back to the screen.
    	Fix bug #127439

--- gnome-panel/panel-toplevel.c
+++ gnome-panel/panel-toplevel.c
@@ -3105,7 +3105,7 @@ panel_toplevel_hide (PanelToplevel    *toplevel,
 
 	if (toplevel->priv->animate && GTK_WIDGET_REALIZED (toplevel))
 		panel_toplevel_start_animation (toplevel);
-	else
+	else if (toplevel->priv->attached)
 		gtk_widget_hide (GTK_WIDGET (toplevel));
 
 	gtk_widget_queue_resize (GTK_WIDGET (toplevel));
@@ -3149,7 +3149,7 @@ panel_toplevel_unhide (PanelToplevel *toplevel)
 
 	if (toplevel->priv->animate && GTK_WIDGET_REALIZED (toplevel))
 		panel_toplevel_start_animation (toplevel);
-	else
+	else if (toplevel->priv->attached)
 		gtk_widget_show (GTK_WIDGET (toplevel));
 
 	gtk_widget_queue_resize (GTK_WIDGET (toplevel));
@@ -3171,7 +3171,8 @@ panel_toplevel_auto_unhide_timeout_handler (PanelToplevel *toplevel)
 	if (toplevel->priv->animating)
 		return TRUE;
 
-	if (!panel_toplevel_contains_pointer (toplevel)) {
+	if (!panel_toplevel_contains_pointer (toplevel) &&
+	    toplevel->priv->auto_hide) {
 		toplevel->priv->unhide_timeout = 0;
 		return FALSE;
 	}
