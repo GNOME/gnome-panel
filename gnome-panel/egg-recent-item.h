240897a4ed5b31628ce530da8e1a109635728745
Vincent Untz <vincent@vuntz.net>
2003-12-13 10:06:42 +0000

    sync with libegg. update to use new API
    2003-12-13  Vincent Untz  <vincent@vuntz.net>
    
    	* egg-recent-item.[ch]:
    	* egg-recent-model.[ch]:
    	* egg-recent-view.[ch]: sync with libegg.
    	* panel-recent.c: (panel_recent_append_documents_menu): update to use
    	new API

--- gnome-panel/egg-recent-item.h
+++ gnome-panel/egg-recent-item.h
@@ -21,10 +21,9 @@ struct _EggRecentItem {
 	gchar *uri;
 	gchar *mime_type;
 	time_t timestamp;
-
+	gchar *app;
+	
 	gboolean private;
-
-	GList *groups;
 	
 	int refcount;
 };
@@ -34,8 +33,8 @@ GType		egg_recent_item_get_type (void) G_GNUC_CONST;
 /* constructors */
 EggRecentItem * egg_recent_item_new (void);
 
-void		egg_recent_item_ref (EggRecentItem *item);
-void		egg_recent_item_unref (EggRecentItem *item);
+EggRecentItem *	egg_recent_item_ref (EggRecentItem *item);
+EggRecentItem *	egg_recent_item_unref (EggRecentItem *item);
 
 /* automatically fetches the mime type, etc */
 EggRecentItem * egg_recent_item_new_from_uri (const gchar *uri);
@@ -53,24 +52,14 @@ time_t egg_recent_item_get_timestamp (const EggRecentItem *item);
 
 G_CONST_RETURN gchar *egg_recent_item_peek_uri (const EggRecentItem *item);
 
-
-/* groups */
-G_CONST_RETURN GList *  egg_recent_item_get_groups (const EggRecentItem *item);
-
-gboolean       egg_recent_item_in_group (const EggRecentItem *item,
-					   const gchar *group_name);
-
-void           egg_recent_item_add_group (EggRecentItem *item,
-					    const gchar *group_name);
-
-void           egg_recent_item_remove_group (EggRecentItem *item,
-					       const gchar *group_name);
-
 void           egg_recent_item_set_private (EggRecentItem *item,
 					      gboolean priv);
 
 gboolean       egg_recent_item_get_private (const EggRecentItem *item);
 
+char          *egg_recent_item_get_application (EggRecentItem *item);
+void           egg_recent_item_set_application (EggRecentItem *item,
+						const char *app);
 
 G_END_DECLS
 
