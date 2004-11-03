eb8759a890c11baee13830ec9bde9e03cb28b119
Jonathan Blandford <jrb@src.gnome.org>
2004-11-03 17:56:36 +0000

    remove gnome-panel-screenshot

--- gnome-panel/screenshot-shadow.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SCREENSHOT_SHADOW_H__
-#define __SCREENSHOT_SHADOW_H__
-
-#include <gtk/gtk.h>
-
-GdkPixbuf *screenshot_add_shadow (GdkPixbuf *src);
-
-#endif /* __SCREENSHOT_SHADOW_H__ */
