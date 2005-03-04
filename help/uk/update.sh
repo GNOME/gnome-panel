7e5b5507803224f218784ea6193cc865cfe26c60
Maxim Dziumanenko <mvd@mylinux.com.ua>
2005-03-04 17:20:51 +0000

    Added Ukrainian translation of the manual.
    2005-03-04  Maxim Dziumanenko <mvd@mylinux.com.ua>
    
    	* Added Ukrainian translation of the manual.

---
+++ help/uk/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xml2po -p uk.po ../C/clock/clock.xml > clock/clock.xml
+xml2po -p uk.po ../C/fish/fish-applet-2.xml > fish/fish-applet-2.xml
+xml2po -p uk.po ../C/window-list/window-list.xml > window-list/window-list.xml
+xml2po -p uk.po ../C/workspace-switcher/workspace-switcher.xml > workspace-switcher/workspace-switcher.xml
