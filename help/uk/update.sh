c64dc6a5848ab02c232fa1a106dfc6eda94c4c29
Maxim Dziumanenko <mvd@mylinux.ua>
2005-08-24 14:03:09 +0000

    Updated Ukrainian doc translation.
    2005-08-24  Maxim Dziumanenko <mvd@mylinux.ua>
    
    	* Updated Ukrainian doc translation.

--- help/uk/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-xml2po -p uk.po ../C/clock/clock.xml > clock/clock.xml
-xml2po -p uk.po ../C/fish/fish-applet-2.xml > fish/fish-applet-2.xml
-xml2po -p uk.po ../C/window-list/window-list.xml > window-list/window-list.xml
-xml2po -p uk.po ../C/workspace-switcher/workspace-switcher.xml > workspace-switcher/workspace-switcher.xml
