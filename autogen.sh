7af0d4444e5c91ae1714af8134b132edd4c1f4bf
Christian Persch <chpe@gnome.org>
2008-01-16 21:48:04 +0000

    Use AM_PROG_CC_C_O. Bug #509781, patch by Christian Kirbach.
    2008-01-16  Christian Persch  <chpe@gnome.org>
    
    	* configure.in: Use AM_PROG_CC_C_O. Bug #509781, patch by Christian
    	Kirbach.
    
    svn path=/trunk/; revision=10743

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="GNOME Panel"
-REQUIRED_AUTOMAKE_VERSION=1.8
+REQUIRED_AUTOMAKE_VERSION=1.9
 
 (test -f $srcdir/configure.in \
   && test -f $srcdir/HACKING \
