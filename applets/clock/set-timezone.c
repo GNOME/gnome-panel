471804462d42d5bd52492be7c56303ec8f0c4b57
Vincent Untz <vuntz@gnome.org>
2008-04-02 15:08:34 +0000

    initialize variables at the beginning of the function, so that we don't
    2008-04-02  Vincent Untz  <vuntz@gnome.org>
    
    	* set-timezone.c: (can_do): initialize variables at the beginning of
    	the function, so that we don't crash because of unitiliazed variables
    	after a goto.
    	Should fix bug #522538, bug #523643.
    
    svn path=/trunk/; revision=10972

--- applets/clock/set-timezone.c
+++ applets/clock/set-timezone.c
@@ -139,6 +139,9 @@ can_do (const gchar *pk_action_id)
         DBusError dbus_error;
 	gint res = 0;
 
+        pk_caller = NULL;
+        pk_action = NULL;
+
 	system_bus = dbus_g_connection_get_connection (get_system_bus ());
 	if (system_bus == NULL)
 		goto out;
@@ -147,9 +150,6 @@ can_do (const gchar *pk_action_id)
 	if (pk_context == NULL)
 		goto out;
 	
-        pk_caller = NULL;
-        pk_action = NULL;
-
         pk_action = polkit_action_new ();
         polkit_action_set_action_id (pk_action, pk_action_id);
 
