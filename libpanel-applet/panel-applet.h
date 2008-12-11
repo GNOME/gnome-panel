b7ed6fbc245e3a1424322d69c5611101342d7f22
Vincent Untz <vuntz@gnome.org>
2008-12-11 00:56:46 +0000

    plug leak in macro in case of error
    2008-12-11  Vincent Untz  <vuntz@gnome.org>
    
    	* panel-applet.h: plug leak in macro in case of error
    
    svn path=/trunk/; revision=11412

--- libpanel-applet/panel-applet.h
+++ libpanel-applet/panel-applet.h
@@ -216,6 +216,7 @@ int main (int argc, char *argv [])						\
 			g_error_free (error);					\
 		} else								\
 			g_printerr ("Cannot parse arguments.\n");		\
+		g_option_context_free (context);				\
 		return 1;							\
 	}									\
 										\
