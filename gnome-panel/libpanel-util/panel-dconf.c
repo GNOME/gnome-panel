bc4816b3a21694909e950996a40f108778f09aa7
Vincent Untz <vuntz@gnome.org>
2011-03-24 21:11:59 +0100

    panel: Add some dconf helper API
    We'll need this later on.

---
+++ gnome-panel/libpanel-util/panel-dconf.c
@@ -0,0 +1,76 @@
+/*
+ * panel-dconf.c: helper API for dconf
+ *
+ * Copyright (C) 2011 Novell, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Authors:
+ *	Vincent Untz <vuntz@gnome.org>
+ */
+
+#include <dconf-client.h>
+#include <dconf-paths.h>
+
+#include "panel-dconf.h"
+
+static DConfClient *
+panel_dconf_client_get (void)
+{
+        return dconf_client_new (NULL, NULL, NULL, NULL);
+}
+
+gboolean
+panel_dconf_recursive_reset (const gchar  *dir,
+                             GError      **error)
+{
+        gboolean     ret;
+        DConfClient *client = panel_dconf_client_get ();
+
+        ret = dconf_client_write (client, dir, NULL,
+                                  NULL, NULL, error);
+
+        g_object_unref (client);
+
+        return ret;
+}
+
+gchar **
+panel_dconf_list_subdirs (const gchar *dir)
+{
+        GArray       *array;
+        gchar       **children;
+        int           len;
+        int           i;
+        DConfClient  *client = panel_dconf_client_get ();
+
+        array = g_array_new (TRUE, TRUE, sizeof (gchar *));
+
+        children = dconf_client_list (client, dir, &len);
+
+        g_object_unref (client);
+
+        for (i = 0; children[i] != NULL; i++) {
+                if (dconf_is_rel_dir (children[i], NULL)) {
+                        char *val = g_strdup (children[i]);
+                        array = g_array_append_val (array, val);
+                }
+        }
+
+        g_strfreev (children);
+
+        return (gchar **) g_array_free (array, FALSE);
+}
