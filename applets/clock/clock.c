c5fedec7c7b4e10d11381fc54c02a19fd23394aa
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2013-12-07 20:15:44 +0200

    clock: workaround gweather crash when closing preferences dialog
    gweather_location_entry_set_location allows using NULL to clear
    entry, but there is bug - #708586. So for now don't use this
    function, and no-op.  That bug is fixed in libgweather 3.10.1
    version.
    
    Known regressions:
     - when adding / removing places, places list is not updated
       until the preferences window is reopened.
     - clock still crashed on some locations
    Document that this is a temporary workaround.
    
    Signed-off-by: Philipp Kaluza <floss@ghostroute.eu>

--- applets/clock/clock.c
+++ applets/clock/clock.c
@@ -1616,7 +1616,8 @@ edit_clear (ClockData *cd)
         GtkWidget *lon_combo = _clock_get_widget (cd, "edit-location-longitude-combo");
 
         /* clear out the old data */
-        gweather_location_entry_set_location (cd->location_entry, NULL);
+        /* TODO: reinstate after bumping to gweather >= 3.10.1 */
+        //gweather_location_entry_set_location (cd->location_entry, NULL);
         gweather_timezone_menu_set_tzid (cd->zone_combo, NULL);
 
         gtk_entry_set_text (GTK_ENTRY (lat_entry), "");
