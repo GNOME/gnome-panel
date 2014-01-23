4afb14167a29dd61f5fdefbce20e45442994f1a1
Daniel Mustieles <daniel.mustieles@gmail.com>
2014-01-23 12:57:54 +0100

    Updated FSF's address

--- applets/clock/system-timezone.c
+++ applets/clock/system-timezone.c
@@ -15,8 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  * 
  * Some code is based on previous code in clock-location.c and on code from
  * tz.c (shipped with version <= 2.22.0). Those files were under the same
