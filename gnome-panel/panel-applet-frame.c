7890290df1d1e57e17a8df870899228443cb2684
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2016-10-07 00:11:14 +0300

    panel-applet-frame: avoid deprecated GdkDeviceManager

--- gnome-panel/panel-applet-frame.c
+++ gnome-panel/panel-applet-frame.c
@@ -338,9 +338,6 @@ panel_applet_frame_button_changed (GtkWidget      *widget,
 	PanelAppletFrame *frame;
 	gboolean          handled = FALSE;
 	guint             modifiers;
-	GdkDisplay       *display;
-	GdkDevice        *pointer;
-	GdkDeviceManager *device_manager;
 
 	frame = PANEL_APPLET_FRAME (widget);
 
@@ -372,10 +369,12 @@ panel_applet_frame_button_changed (GtkWidget      *widget,
 	case 3:
 		if (event->type == GDK_BUTTON_PRESS ||
 		    event->type == GDK_2BUTTON_PRESS) {
+			GdkDisplay *display;
+			GdkSeat *seat;
+
 			display = gtk_widget_get_display (widget);
-			device_manager = gdk_display_get_device_manager (display);
-			pointer = gdk_device_manager_get_client_pointer (device_manager);
-			gdk_device_ungrab (pointer, GDK_CURRENT_TIME);
+			seat = gdk_display_get_default_seat (display);
+			gdk_seat_ungrab (seat);
 
 			if (modifiers == panel_bindings_get_mouse_button_modifier_keymask ())
 				PANEL_APPLET_FRAME_GET_CLASS (frame)->popup_edit_menu (frame,
