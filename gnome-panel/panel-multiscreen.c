8c8ac2317c38c2132b3b28104018fe1665a23832
Sebastian Geiger <sbastig@gmx.net>
2020-02-25 00:04:49 +0100

    multiscreen: make panel_multiscreen_reinit static

--- gnome-panel/panel-multiscreen.c
+++ gnome-panel/panel-multiscreen.c
@@ -41,6 +41,8 @@ static gboolean       have_randr  = FALSE;
 static gboolean       have_randr_1_3 = FALSE;
 static guint          reinit_id   = 0;
 
+static void panel_multiscreen_reinit (void);
+
 static gboolean
 _panel_multiscreen_output_should_be_first (Display       *xdisplay,
 					   RROutput       output,
@@ -446,7 +448,7 @@ panel_multiscreen_init (void)
 	initialized = TRUE;
 }
 
-void
+static void
 panel_multiscreen_reinit (void)
 {
 	GdkScreen *screen;
