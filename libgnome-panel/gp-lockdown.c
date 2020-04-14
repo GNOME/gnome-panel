e31cb41a53bcea42d5fef5f8b8578587e250417d
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2020-04-14 15:25:39 +0300

    libgnome-panel: add GpLockdownFlags

---
+++ libgnome-panel/gp-lockdown.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2020 Alberts Muktupāvels
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+#include "gp-lockdown.h"
+
+/**
+ * SECTION: gp-lockdown
+ * @title: GpLockdown
+ * @short_description: lockdown enum
+ * @include: libgnome-panel/gp-lockdown.h
+ *
+ * A #GpLockdownFlags.
+ */
