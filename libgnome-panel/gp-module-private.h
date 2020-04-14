40923e68c9028135e54e7af195686fe1c90f36d7
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2020-04-14 18:41:35 +0300

    libgnome-panel: add gp_module_is_applet_disabled

--- libgnome-panel/gp-module-private.h
+++ libgnome-panel/gp-module-private.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2018 Alberts Muktupāvels
+ * Copyright (C) 2016-2020 Alberts Muktupāvels
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -33,40 +33,46 @@ typedef enum
 #define GP_MODULE_ERROR gp_module_error_quark ()
 GQuark gp_module_error_quark (void);
 
-GpModule            *gp_module_new_from_path         (const gchar   *path);
+GpModule            *gp_module_new_from_path          (const gchar      *path);
 
-const gchar         *gp_module_get_id                (GpModule      *module);
+const gchar         *gp_module_get_id                 (GpModule         *module);
 
-const gchar         *gp_module_get_version           (GpModule      *module);
+const gchar         *gp_module_get_version            (GpModule         *module);
 
-const gchar * const *gp_module_get_applets           (GpModule      *module);
+const gchar * const *gp_module_get_applets            (GpModule         *module);
 
-GpAppletInfo        *gp_module_get_applet_info       (GpModule      *module,
-                                                      const gchar   *applet,
-                                                      GError       **error);
+GpAppletInfo        *gp_module_get_applet_info        (GpModule         *module,
+                                                       const gchar      *applet,
+                                                       GError          **error);
 
-const gchar         *gp_module_get_applet_id_from_iid (GpModule     *module,
-                                                       const gchar  *old_iid);
+const gchar         *gp_module_get_applet_id_from_iid (GpModule         *module,
+                                                       const gchar      *old_iid);
 
-GtkWidget           *gp_module_get_standalone_menu    (GpModule     *module,
-                                                       gboolean      enable_tooltips,
-                                                       gboolean      locked_down,
-                                                       guint         menu_icon_size);
+GtkWidget           *gp_module_get_standalone_menu    (GpModule         *module,
+                                                       gboolean          enable_tooltips,
+                                                       gboolean          locked_down,
+                                                       guint             menu_icon_size);
 
-GpApplet            *gp_module_applet_new             (GpModule     *module,
-                                                       const gchar  *applet,
-                                                       const gchar  *settings_path,
-                                                       GVariant     *initial_settings,
-                                                       GError      **error);
+GpApplet            *gp_module_applet_new             (GpModule         *module,
+                                                       const gchar      *applet,
+                                                       const gchar      *settings_path,
+                                                       GVariant         *initial_settings,
+                                                       GError          **error);
 
-GtkWidget           *gp_module_create_about_dialog    (GpModule     *module,
-                                                       GtkWindow    *parent,
-                                                       const char   *applet);
+GtkWidget           *gp_module_create_about_dialog    (GpModule         *module,
+                                                       GtkWindow        *parent,
+                                                       const char       *applet);
 
-void                 gp_module_show_help              (GpModule     *module,
-                                                       GtkWindow    *parent,
-                                                       const char   *applet,
-                                                       const char   *page);
+void                 gp_module_show_help              (GpModule         *module,
+                                                       GtkWindow        *parent,
+                                                       const char       *applet,
+                                                       const char       *page);
+
+gboolean             gp_module_is_applet_disabled     (GpModule         *module,
+                                                       const char       *applet,
+                                                       const char       *backend,
+                                                       GpLockdownFlags   lockdowns,
+                                                       char            **reason);
 
 G_END_DECLS
 
